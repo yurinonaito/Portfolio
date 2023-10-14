e0fab80c26f8c1bd760e96a7ac67c2f6fe9a826d
Yurino Naito <yurino.naito@gmail.com>
2023-10-14 03:41:44 +0000

    レイアウト修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Portfolio
     config.load_defaults 6.1
     
     config.i18n.default_locale = :ja
+    
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
