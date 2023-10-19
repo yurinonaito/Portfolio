46d7505df0feb0bc551642bf434c748ab0a55f77
Yurino Naito <yurino.naito@gmail.com>
2023-10-19 13:08:27 +0000

    グループチャットの通知機能追加・動作確認済

--- app/models/group_chat.rb
+++ app/models/group_chat.rb
@@ -2,17 +2,22 @@ class GroupChat < ApplicationRecord
   belongs_to :user
   belongs_to :group_room
   
-  # has_one :notification, as: :subject, dependent: :destroy
+  has_one :notification, as: :subject, dependent: :destroy
 
-  # after_create_commit :create_notifications
+  after_create_commit :create_notifications
   
    validates :message, presence: true, length: { maximum: 140 }
    
-  #   private
+    private
   
-  # def create_notifications
-  #   entry = Entry.where.not(user_id: chat.user.id).where().first
-  #   Notification.create(subject: self, user: entry.user, action_type: :chated_to_me)
-  # end
-  
+      def create_notifications
+      # 1. グループチャットのルームにエントリーしているユーザーのリストを取得
+      group_entry_users = group_room.group_entries.where.not(user_id: user.id).map(&:user)
+    
+      # 2. 取得したユーザーリストに通知を作成
+      group_entry_users.each do |group_entry_user|
+        Notification.create(subject: self, user: group_entry_user, action_type: :groupchated_to_me)
+      end
+    end
+
 end
